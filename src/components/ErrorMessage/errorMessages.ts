14a1d18150ccc4c772dfe38baebf2e7eb0e638c6
Fabrice Million <fabrice.million@gmail.com>
2024-01-25 02:04:39 +1100

    Upd: added type for error messages

--- src/components/ErrorMessage/errorMessages.ts
+++ src/components/ErrorMessage/errorMessages.ts
@@ -1,4 +1,9 @@
-const errorMessages = {
+type ErrorMessages = {
+    shipments: string;
+    events: string;
+  };
+
+const errorMessages: ErrorMessages = {
     shipments: 'Sorry an error has occured, please try again later',
     events:'Sorry an error has occured, please try again later'
 }
